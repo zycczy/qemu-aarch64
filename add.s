5015f3b7232c2b3461ec712b53a9c1b0dd55549f
zhaochen <zhaochen@codeaurora.org>
2019-04-04 17:55:44 +0800

    update mluti core boot, but has bug, maybe psci
    Signed-off-by: zhaochen <zhaochen@codeaurora.org>

--- add.s
+++ add.s
@@ -1,6 +1,8 @@
 .text
 .global  _start
 _start:                       // Label, not really required
+	SEV
+	SEV
 	MRS   x0, CurrentEL
 	CMP   x0, #8	      // if cureent el is el2
 	B.EQ  el2_init
@@ -8,9 +10,10 @@ _start:                       // Label, not really required
 _stop:   b _stop               //Infinite loop to stop execution
 
 el2_init:
-	MSR     SPsel, #1                       // We want to use SP_EL{1,2}
+	MSR   SPsel, #1                       // We want to use SP_EL{1,2}
 	MRS   x1, MIDR_EL1
 	MRS   x2, MPIDR_EL1
 	AND   x3, x2, #0xff00
 	AND   x4, x2, #0xff
+	STR   x2, [x2]
 	B     _stop
